434f292e2225e324b8aaf2bb85c76084c2578a07
yesinaib <yesinaib@gmail.com>
2020-03-25 22:04:00 -0600

    practica javascript notificaciones

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+nombre=prompt("¿Cómo te llamas?");
+edad=prompt("¿Qué edad tienes?");
+
+if (edad >= 18) {
+	mostrarAlerta("Hola " + nombre + " puedes pasar", "success");	
+} else {
+	mostrarAlerta("Hola " + nombre + " no puedes pasar", "danger");
+}
+
+function mostrarAlerta(mensaje,tipo){
+$(".alert").text(mensaje);
+$(".alert").addClass("alert-"+ tipo);
+}
+*/
